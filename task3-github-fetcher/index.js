3d2a9364d7a079b322be66fa9dade2f6cf84be9c
Kunal Khandelwal <kunalkhandelwal5604@gmail.com>
2025-09-26 23:28:15 +0530

    Fix for vercel deploy

--- task3-github-fetcher/index.js
+++ task3-github-fetcher/index.js
@@ -1,27 +1,28 @@
-import express from "express"
-import axios from "axios"
-const app = express()
-const port = 3000;
-app.use(express.urlencoded({ extended: true }))
-app.use(express.json())
+const express = require("express");
+const axios = require("axios");
+const path = require("path");
 
-app.set("view engine", "ejs")
+const app = express();
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "..", "views"));
+
+app.use(express.urlencoded({ extended: true }));
+
 app.get("/", (req, res) => {
-  res.render("home", { user: null, error: null })
-})
+  res.render("home", { user: null, error: null });
+});
 
 app.post("/", async (req, res) => {
-  const { username } = req.body
+  const username = req.body.username;
+
   try {
-    const { data } = await axios.get(`https://api.github.com/users/${username}`)
-    res.render("home", { user: data, error: null })
-  } catch (e) {
-    res.render("home", { user: null, error: "User not found" })
+    const response = await axios.get(`https://api.github.com/users/${username}`);
+    res.render("home", { user: response.data, error: null });
+  } catch (err) {
+    res.render("home", { user: null, error: "User not found" });
   }
-})
+});
 
 
-
-app.listen(port, () => {
-  console.log(`Server is listening on port ${port}`)
-})
+module.exports = app;
